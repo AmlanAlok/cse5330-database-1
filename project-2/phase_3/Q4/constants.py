93108ef204b47287f2171ea2908f2bfa43344304
Amlan Alok <amlanalok@gmail.com>
2022-11-19 21:16:49 -0600

    Phase 3: constants file added

---
+++ project-2/phase_3/Q4/constants.py
@@ -0,0 +1,41 @@
+
+HOSTNAME = 'hostname'
+USERNAME = 'username'
+PASSWORD = 'password'
+DATABASE = 'database'
+
+LOCAL_DB = {
+    HOSTNAME: 'localhost',
+    USERNAME: 'root',
+    PASSWORD: '',
+    DATABASE: 'axa5861'
+}
+
+OMEGA_DB = {
+    HOSTNAME: '10.208.63.68',
+    USERNAME: 'axa5861',
+    PASSWORD: 'ArlingtonTexas',
+    DATABASE: 'axa5861'
+}
+
+TABLE_NAME = 'table_name'
+COL_NAME = 'col_name'
+
+ACTIVE = 'active'
+INACTIVE = 'inactive'
+MEMBER_STATUS = 'member_status'
+MEMBER_STATUS_DICT = {
+    TABLE_NAME: MEMBER_STATUS,
+    COL_NAME: MEMBER_STATUS
+}
+
+STANDARD = 'standard'
+PROFESSOR = 'professor'
+MEMBER_TYPE = 'member_type'
+MEMBER_TYPE_DICT = {
+    TABLE_NAME: MEMBER_TYPE,
+    COL_NAME: MEMBER_TYPE
+}
+
+MEMBERSHIP_LENGTH = 4
+INIT_BORROWED_COUNT = 0
