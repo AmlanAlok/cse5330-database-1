ce0bf27aced390a875e696b184e84d74f89b8f90
Amlan Alok <amlanalok@gmail.com>
2022-11-22 18:39:39 -0600

    Phase 3: drop script added

---
+++ project-2/phase_3/drop_tables/drop_library.sql
@@ -0,0 +1,25 @@
+use axa5861;
+
+drop table interested_to_acquire;
+drop table reason;
+
+drop table book_for_lending;
+
+drop table notification;
+
+drop table book_member;
+
+drop table member;
+drop table member_status;
+drop table member_type;
+
+drop table book;
+
+drop table author;
+drop table binding;
+drop table book_category;
+drop table lang;
+drop table subject_area;
+
+drop table employee;
+drop table staff_position;
